ba2644b226fe968f1bb39fa0ebe2bc93146b4baf
Yuta_Ushizima <contact@yuta-u.com>
2022-04-30 12:30:34 +0900

    feat: Display artilces with dummy data

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import ListItem from './components/ListItem';
+import articles from './dummies/articles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+      data={articles}
+      renderItem={(({item}) =>
+        <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author} />
+        )}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +21,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
